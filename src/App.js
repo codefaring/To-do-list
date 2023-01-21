510c23b026f08eead80037f1d6a4fc43c4ded78a
codefaring <codefaring@gmail.com>
2023-01-21 23:43:52 +0900

    test1 to-do-list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [lists, setlists] = useState(['장보기', '공부하기']);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {lists.map((list) => (
+          <li>{list}</li>
+        ))}
+      </ul>
+      <button
+        onClick={() => {
+          const todo = prompt('변경할 할일 목록을 입력하세요');
+          setlists(
+            lists.map((list) => {
+              if (list === todo) {
+              }
+              return list;
+            })
+          );
+        }}
+      >
+        할일 변경하기
+      </button>
+      <button
+        onClick={() => {
+          const addTodo = prompt('추가하고 싶은 할일은 무엇인가요?');
+          setlists((lists) => [...lists, addTodo]);
+        }}
+      >
+        할일 추가하기
+      </button>
+      <button>할일 삭제하기</button>
     </div>
   );
 }
-
-export default App;
